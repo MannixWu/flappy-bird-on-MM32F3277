7fc96dce217470c15d321e77672bd0d6664ef4d8
MannixWu <steveandjobs3068758340@gmail.com>
2021-08-08 11:38:48 +0800

    优化了代码结构

--- Project/MDK/res.h
+++ Project/MDK/res.h
@@ -1,4 +1,15 @@
 #ifndef _RES_H_
 #define _RES_H_
 //uint16 backGround[HEIGHT][WIDTH];
+
+extern uint16 backGround[50][135];
+extern uint16 land[8][135];
+extern uint16 bird_0[24][34];
+extern uint16 bird_1[24][34];
+extern uint16 bird_2[24][34];
+extern uint16 pipe[32][52];
+extern uint16 gameover[20][94];
+extern uint16 numbers[10][28][18];
+
+
 #endif
